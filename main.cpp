9c819cc4a1e7b778c5629ff1410191a4e0c4defc
jungsa <106296196+knkevol@users.noreply.github.com>
2025-09-29 13:57:10 +0900

    플레이어 이동 및 몬스터표출

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Windows.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -12,31 +13,89 @@ struct FCharacter
 FCharacter Player;
 FCharacter Monster;
 
-FCharacter Characters[2];
+FCharacter Characters[2]; //구조체 배열
 
-void Render(FCharacter Indata)
+void RenderCharacter(FCharacter Indata)
 {
-	//for - ALL
-	COORD Cur;
-	Cur.X = Indata.X;
-	Cur.Y = Indata.Y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-	cout << Indata.Shape;
+		COORD Cur;
+		Cur.X = (SHORT)Indata.X;
+		Cur.Y = (SHORT)Indata.Y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+		cout << Indata.Shape;
+}
+
+void Render()
+{
+	for (int i = 0; i < 2; i++)
+	{
+		RenderCharacter(Characters[i]);
+	}
+}
+
+void Init()
+{
+	//Load > 파일입출력으로 제작
+	Characters[0].X = 1;
+	Characters[0].Y = 1;
+	Characters[0].Shape = 'P';
+
+	Characters[1].X = 10;
+	Characters[1].Y = 10;
+	Characters[1].Shape = 'M';
+}
+int KeyCode = 0;
+void Input()
+{
+	KeyCode = _getch();
+}
+
+void MovePlayer()
+{
+	bool bIsPlaying = true;
+	if (KeyCode == 'w')
+	{
+		Characters[0].Y--;
+	}
+	else if (KeyCode == 's')
+	{
+		Characters[0].Y++;
+	}
+	else if (KeyCode == 'a')
+	{
+		Characters[0].X--;
+	}
+	else if (KeyCode == 'd')
+	{
+		Characters[0].X++;
+	}
+	else if (KeyCode == 'q')
+	{
+		bIsPlaying = false;
+	}
+	system("cls");
+}
+
+void MoveMonster()
+{
+
+}
+
+void Tick()
+{
+	MovePlayer();
+	MoveMonster();
 }
 
 int main()
 {
-	//Load > 파일입출력으로 제작
-	Player.X = 1;
-	Player.Y = 1;
-	Player.Shape = 'P';
+	Init(); //UE5 == BeginPlay
 
-	Monster.X = 10;
-	Monster.Y = 10;
-	Monster.Shape = 'M';
-
-	Render(Player);
-	Render(Monster);
-
+	while (true)
+	{
+		Input();
+		Tick();
+		Render();
+	}
+	
 	return 0;
 }
